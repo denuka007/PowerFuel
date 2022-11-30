98814018417518b01cefae7b6babb67adfb53c17
SpaceSeeker101 <86275338+SpaceSeeker101@users.noreply.github.com>
2022-11-30 14:30:21 +0530

    first and completed commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Modules\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $role=Auth::user()->user_role;
+
+        if($role=='1')
+        {
+            return view('admin');
+        }
+        elseif($role=='2')
+        {
+            return view('seller');
+        }
+        else
+        {
+            return view('dashboard');
+        }
+    }
+
+    public function addseller(Request $request)
+    {
+        $data=new user;
+        $data->name=$request->name;
+        $data->email=$request->email;
+        $data->password=bcrypt($request->password);
+
+        $data->user_role='2';
+        $data->save();
+
+        return redirect()->back();
+    }
+}
---
+++ resources/views/seller.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+
+    <div>
+        <h1>Seller Dashboard</h1>
+    </div>
+    
+</x-app-layout>
